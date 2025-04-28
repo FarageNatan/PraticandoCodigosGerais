2ae09370dfc9306b74c67098e07ead3e7c23d2c1
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2025-04-28 19:33:09 -0300

    Create EuPossoAdvinharAEstruturaDeDados.java

---
+++ EuPossoAdvinharAEstruturaDeDados.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class EuPossoAdvinharAEstruturaDeDados{
+    static Scanner entrada = new Scanner(System.in); 
+
+    public static int numeroCasos(){
+        int valor= 0;
+        valor = entrada.nextInt();
+        return valor;
+    }
+
+    public static void determinandoComandos(int valor){
+        int num = 0, num2 = 0;
+        for(int i = 0; i < valor; i++){
+            num = entrada.nextInt();
+            
+        }
+        
+    }
+
+
+
+
+    public static void main(String[] args) {
+        
+    }
+
+}
