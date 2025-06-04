5d3cf53558a3c37d07d643f26c8b42302f6216cf
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2025-06-04 12:17:12 -0300

    correcoes

--- AreaCirculo.java
+++ AreaCirculo.java
@@ -1,30 +1,22 @@
 import java.io.IOException;
 import java.util.*;
  
-/**
- * IMPORTANT: 
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
-public class AreaCirculo {
-    static Scanner sc = new Scanner(System.in);
+public class AreaCirculo { // Nome da classe corrigido
     
-    public static double areaCirculo(double raio){
-        double area = raio * raio * 3.14160;
+    public static double areaCirculo(double raio) {
+        double area = raio * raio * 3.14159; 
         return area; 
     }
     
     public static void main(String[] args) {
-
-        double resultado = areaCirculo(2.00);
-
-        double res2 = areaCirculo(100.64);
+        Scanner sc = new Scanner(System.in);
         
-        double res3 = areaCirculo(150.00);
+        double valor = sc.nextDouble();
+        
+        double resultado = areaCirculo(valor);
 
-        System.out.println("A=" + resultado);
-        System.out.println("A=" + res3);
-        System.out.println("A=" + res2);
+        System.out.printf("A=%.4f%n", resultado);
+    
+        sc.close();
     }
 }
