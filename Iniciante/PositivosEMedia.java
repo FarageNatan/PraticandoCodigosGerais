8fc6353da8ac363aa7602991e1b065b15e4fc582
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2025-08-12 22:51:00 -0300

    treinando compilar e executar pelo terminal + exercicio

--- Iniciante/PositivosEMedia.java
+++ Iniciante/PositivosEMedia.java
@@ -5,9 +5,20 @@ import java.util.Scanner;
 public class PositivosEMedia {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        double[] valores = new double[5];
-        for(int i = 0; i < 5; i++){
+        double[] valores = new double[6];
+        double soma = 0, media = 0;
+        int positivos =0;
+        for(int i = 0; i < 6; i++){
             valores[i] = sc.nextDouble();
         }
+        for(int j = 0; j < 6; j++){
+            if(valores[j] > 0){
+                soma += valores[j];
+                positivos++;
+            }
+        }
+        media = soma / positivos;
+        System.out.println(positivos + " valores positivos");
+        System.out.println(media);
     }
 }
