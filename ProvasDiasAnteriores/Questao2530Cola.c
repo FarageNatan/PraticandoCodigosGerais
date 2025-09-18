8cd013cd815923a0fd5a74a47f5679f08fdc9606
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2025-09-18 15:24:04 -0300

    mais exercicio prova

--- ProvasDiasAnteriores/Questao2530Cola.c
+++ ProvasDiasAnteriores/Questao2530Cola.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int numRespostas(){
-    int resp;
-    scanf("%d", &resp);
-    return resp;
-}
-
-int* respostasDasQuestoes(int n){
-    int* respostas = (int*)malloc(sizeof(int) * n);
-    for(int i = 0; i < n; i++){
-        scanf("%d", &respostas[i]);
-    }
-    return respostas;
-}
-
-int* confereResposta(int* respostasRicardinho, int* respostasJuan, int tam1, int tam2){
-    int* comparaIgual = (int*)malloc(sizeof(int) * tam1);
-    
-
-
-}
-
-
-
 int main(){
+    int qtd_resp1, qtd_resp2;
+    while(scanf("%d %d", &qtd_resp1, &qtd_resp2) != EOF){
+        printf("%i %i\n", qtd_resp1, qtd_resp2);
+        int* resp1 = (int*)malloc(sizeof(int) * qtd_resp1);
+        int* resp2 = (int*)malloc(sizeof(int) * qtd_resp2);
+        for(int i = 0; i < qtd_resp1; i++){
+            int valor;
+            scanf("%d", &resp1[i]);
+        }
+        for(int j = 0; j < qtd_resp2; j++){
+            scanf("%d", &resp2[j]);
+        }
+        int p = 0;
+        for(int v = 0; v < qtd_resp1; v++){
+            if(resp2[p] == resp1[v]){
+                p++;
+            }
+        }
+        if(p == qtd_resp2){
+            printf("sim\n");
+        }else{
+            printf("nao\n");
+        }
 
-
-
+    }
     return 0;
 }
