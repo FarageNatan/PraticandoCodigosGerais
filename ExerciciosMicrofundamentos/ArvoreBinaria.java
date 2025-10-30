5a540dd055fc5842c69879b5966b9e520cd7abd9
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2025-10-30 09:58:58 -0300

    implementacao pesquisa e correcoes

--- ExerciciosMicrofundamentos/ArvoreBinaria.java
+++ ExerciciosMicrofundamentos/ArvoreBinaria.java
@@ -16,13 +16,16 @@ class No{
 class ArvoreB{
     No raiz;
     //---- Construtor -----
+    
     ArvoreB(){
         raiz = null;
     }
+
     //---- Gatilhos -------
-    private void inserir(int x) throws Exception{
+    public void inserir(int x) throws Exception{
         raiz = inserir(x, raiz);
     }
+
     private void inserirPai(int x) throws Exception{
         if(raiz == null){
             raiz = new No(x);
@@ -34,36 +37,30 @@ class ArvoreB{
             throw new Exception("Erro!");
         }
     }
-    private boolean pesquisar(int x){
+
+    public boolean pesquisar(int x) throws Exception{
+        boolean resp = pesquisar(x, raiz);
+        return resp;
+    }
+
+    public void remover(int x){
 
     }
-    private void caminharCentral(No i){
-        if(i != null){
-            caminharCentral(i.esq);
-            System.out.println(i.elemento + " ");
-            caminharCentral(i.dir);
-        }
-    }
-    private void caminharPos(No i){
-        if(i != null){
-            caminharPos(i.esq);
-            caminharPos(i.dir);
-            System.out.println(i.elemento + " ");
-        }
-    }
-    private void caminharPre(No i){
-        if(i != null){
-            System.out.println(i.elemento + " ");
-            caminharPre(i.esq);
-            caminharPre(i.dir);
-        }
-    }
-    private void remover(int x){
 
+    public void caminharPre(){
+        caminharPre(raiz);
+    }
+
+    public void caminharCentral(){
+        caminharCentral(raiz);
+    }
+
+    public void caminharPos(){
+        caminharPos(raiz);
     }
 
     //----- Funcoes ------
-    public No inserir(int x, No i) throws Exception{
+    private No inserir(int x, No i) throws Exception{
         if(i == null){
             i = new No(x);
         }else if(x < i.elemento){
@@ -76,7 +73,7 @@ class ArvoreB{
         return i;
     }
 
-    void inserirPai(int x, No i, No pai) throws Exception{
+    private void inserirPai(int x, No i, No pai) throws Exception{
         if(i ==  null){ //Posicao que devemos inserir
             if(x < pai.elemento){ //Nao temos nenhuma referencia se é maior ou menor que o pai, por isso devemos testar novamente.
                 pai.esq = new No(x);
@@ -91,6 +88,49 @@ class ArvoreB{
             throw new Exception("Erro!");
         }
     }
+
+    private boolean pesquisar(int x, No i) throws Exception{
+        boolean encontrado = false;
+        if(i == null){
+            encontrado = false;
+            throw new Exception("Erro!");
+        }else{
+            if(x < i.elemento){
+                pesquisar(x, i.esq);
+            }else if(x > i.elemento){
+                pesquisar(x, i.dir);
+            }else{
+                encontrado = true;
+            }
+        }
+        return encontrado;
+    }
+
+    private void caminharCentral(No i){
+        if(i != null){
+            caminharCentral(i.esq);
+            System.out.println(i.elemento + " ");
+            caminharCentral(i.dir);
+        }
+    }
+    
+    private void caminharPos(No i){
+        if(i != null){
+            caminharPos(i.esq);
+            caminharPos(i.dir);
+            System.out.println(i.elemento + " ");
+        }
+    }
+
+    private void caminharPre(No i){
+        if(i != null){
+            System.out.println(i.elemento + " ");
+            caminharPre(i.esq);
+            caminharPre(i.dir);
+        }
+    }
+
+
 }
 
 public class ArvoreBinaria {
