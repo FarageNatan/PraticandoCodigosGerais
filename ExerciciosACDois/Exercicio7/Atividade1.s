40a956240a9bc6831a8b74e504cfef9469605c47
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2025-12-08 21:04:51 -0300

    Atividade 1 - Exercicio 7 - Correcao

--- ExerciciosACDois/Exercicio7/Atividade1.s
+++ ExerciciosACDois/Exercicio7/Atividade1.s
@@ -12,13 +12,12 @@ main:
     lw t0, s0, 0
     add t1, t1, t0 #t1 vai armazenar a soma dos elementos do vetor
     #Carrega no registrador s0 o endereco de memoria que esta guardado o valor 20 e atualiza t0 com a soma
-    lui s0, %hi(vetor)
-    addi s0, s0, %lo(vetor)
     lw t0, s0, 4
     add t1, t1, t0 #atuliza o somador
     #Procura o proximo elemento e som
-    lui s0, %hi(vetor)
-    addi s0, s0, %lo(vetor)
     lw t0, s0, 8
     add t1, t1, t0 #atualiza o somador
+    mv a0, t1
+    addi t0, zero, 1
+    ecall
     ret
